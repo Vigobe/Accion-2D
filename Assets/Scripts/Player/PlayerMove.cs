59267684b13fd5c0fe464d49f619a7d9a92a30a1
Vigobe <147854789+Vigobe@users.noreply.github.com>
2024-03-02 09:53:55 -0400

    Animaciones
    Se agregan animaciones al personaje y se crean nuevos players

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,27 +1,46 @@
-    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     [Header("Config")]
-    [SerializeField] private float velocidad = 10f;
+    [SerializeField] private float velocidadMovimiento = 10f;
 
-    private Rigidbody2D rigidbody2D;
+    [Header("Dash")]
+    [SerializeField] private float velocidadDash;
+    [SerializeField] private float tiempoDash;
+    [SerializeField] private float transparencia; 
+    
+    private Rigidbody2D rb2D;
     private PlayerAcciones acciones;
+    private SpriteRenderer spriteRenderer;
+    private bool usandoDash;
+
+    private float velocidadActual;
     private Vector2 direccionMovimiento;
 
 
     private void Awake()
     {
-        acciones  = new PlayerAcciones();
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        acciones  = new PlayerAcciones(); //Herencia clase PlayerAcciones
+        rb2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 
+
+    }
+
+    private void Start()
+    {
+        velocidadActual = velocidadMovimiento;
+        acciones.Movimiento.Dash.performed += ctx => Dash();
     }
 
     private void Update()
     {
         CapturarInput();
+        RotarPlayer();
+
     }
 
     private void FixedUpdate()
@@ -31,7 +50,47 @@ public class PlayerMove : MonoBehaviour
 
     private void MoverPlayer()
     {
-        rigidbody2D.MovePosition(rigidbody2D.position + direccionMovimiento * (velocidad * Time.fixedDeltaTime));
+        GetComponent<Rigidbody2D>().MovePosition(GetComponent<Rigidbody2D>().position + direccionMovimiento * (velocidadActual * Time.fixedDeltaTime));
+    }
+
+    private void Dash()
+    {
+        if (usandoDash)
+        {
+            return;
+        }
+        usandoDash = true;
+        StartCoroutine(IEDash());
+
+    }
+    private IEnumerator IEDash()
+    {
+        velocidadActual = velocidadDash;
+        ModificarSpriteRenderer(transparencia);
+        yield return new WaitForSeconds(tiempoDash);
+        ModificarSpriteRenderer(valor: 1f);
+        velocidadActual = velocidadMovimiento;
+        usandoDash = false;
+
+    }
+    private void ModificarSpriteRenderer(float valor) 
+    {
+        Color color = spriteRenderer.color;
+        color = new Color(color.r, color.g,color.b,valor);
+        spriteRenderer.color = color;
+
+    }
+
+    private void RotarPlayer()
+    {
+        if (direccionMovimiento.x >= 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (direccionMovimiento.x < 0f)
+        {
+            spriteRenderer.flipX = true;
+        } 
     }
     private void CapturarInput()
     {
